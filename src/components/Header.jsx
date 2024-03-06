ca7fe483bcea0cc7793a734eff161b48be8266d3
Kron1410 <anmaslen@gmail.com>
2024-03-06 16:00:19 +0400

    Начало разработки. Создание разметки

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src = "https://w7.pngwing.com/pngs/656/127/png-transparent-chechnya-badge-encapsulated-postscript-seal-miscellaneous-animals-drawing-thumbnail.png" />
+        </header>
+    );
+};
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img width="1000" src = "https://wallpapers.com/images/hd/dual-monitor-background-eycrdqoprfo59obs.jpg" />
+            </div>
+            <div>
+                ava + desc
+                <img src="https://sun9-33.userapi.com/c854528/v854528061/20a930/mdBGplDIEzk.jpg?ava=1" />
+            </div>
+
+            <div>
+                My post
+                <div>
+                    New Post
+                </div>
+            </div>
+
+            <div>          
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
